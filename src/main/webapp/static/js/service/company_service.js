2c977c212cef5b751a3358f546238ee987af9e04
Simon N <simtuje@gmail.com>
2016-02-20 08:15:00 +0300

    update to JavaScipt file

--- src/main/webapp/static/js/service/company_service.js
+++ src/main/webapp/static/js/service/company_service.js
@@ -1,48 +1,44 @@
 'use strict';
 
-App.factory('CompanyService', ['$http', '$q', function($http, $q){
+App.factory('CompanyService', ['$http', '$q', function($http, $q) {
+    return {
+        fetchAllCompanies: function() {
+            return $http.get('http://localhost:8080/CompanyRestService/restservice/companies')
+                .then(
+                    function(response) {
+                        return response.data;
+                    },
+                    function(errResponse) {
+                        console.error('Error while fetching companies');
+                        return $q.reject(errResponse);
+                    }
+                );
+        },
 
-	return {
-		
-			fetchAllCompanies: function() {
-					return $http.get('http://localhost:8080/CompanyRestService/restservice/companies')
-							.then(
-									function(response){
-										return response.data;
-									}, 
-									function(errResponse){
-										console.error('Error while fetching companies');
-										return $q.reject(errResponse);
-									}
-							);
-			},
-		    
-		    createCompany: function(company){
-					return $http.post('http://localhost:8080/CompanyRestService/restservice/company', company)
-							.then(
-									function(response){
-										return response.data;
-									}, 
-									function(errResponse){
-										console.error('Error while creating company');
-										return $q.reject(errResponse);
-									}
-							);
-		    },
-		    
-		    updateCompany: function(company, id){
-					return $http.put('http://localhost:8080/CompanyRestService/restservice/company/'+id, company)
-							.then(
-									function(response){
-										return response.data;
-									}, 
-									function(errResponse){
-										console.error('Error while updating company');
-										return $q.reject(errResponse);
-									}
-							);
-			}
-		
-	};
+        createCompany: function(company) {
+            return $http.post('http://localhost:8080/CompanyRestService/restservice/company', company)
+                .then(
+                    function(response) {
+                        return response.data;
+                    },
+                    function(errResponse) {
+                        console.error('Error while creating company');
+                        return $q.reject(errResponse);
+                    }
+                );
+        },
 
+        updateCompany: function(company, id) {
+            return $http.put('http://localhost:8080/CompanyRestService/restservice/company/'+id, company)
+                .then(
+                    function(response) {
+                        return response.data;
+                    },
+                    function(errResponse) {
+                        console.error('Error while updating company');
+                        return $q.reject(errResponse);
+                    }
+                );
+        }
+    };
 }]);
