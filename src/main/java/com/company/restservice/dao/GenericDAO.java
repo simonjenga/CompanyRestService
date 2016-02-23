d7204188806a73e5bdc3d12deede7db3de47ef5b
Simon N <simtuje@gmail.com>
2016-02-23 10:10:00 +0300

    formatting code & javadocs

--- src/main/java/com/company/restservice/dao/GenericDAO.java
+++ src/main/java/com/company/restservice/dao/GenericDAO.java
@@ -4,27 +4,33 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * This abstract class is a generic Data Access Object (DAO) which defines the basic common
+ * methods for hibernate's <code>SessionFactory</code> entities in the domain model.
+ *
+ * @author Simon Njenga
+ * @since 0.1
+ */
 public abstract class GenericDAO {
 
-	@Autowired
-	private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-	/**
-	 * Returns the CurrentSession for direct reference from subclasses.
-	 * 
-	 * @return CurrentSession
-	 */
-	protected Session getCurrentSession() {
-		return sessionFactory.getCurrentSession();
-	}
-	
-	/**
-	 * Returns the OpenSession for direct reference from subclasses.
-	 * 
-	 * @return OpenSession
-	 */
-	protected Session getOpenSession() {
-		return sessionFactory.openSession();
-	}
+    /**
+     * Returns the CurrentSession for direct reference from subclasses.
+     *
+     * @return The current <code>Session</code> in hibernate's <code>SessionFactory</code>
+     */
+    protected Session getCurrentSession() {
+        return sessionFactory.getCurrentSession();
+    }
 
+    /**
+     * Returns the OpenSession for direct reference from subclasses.
+     *
+     * @return The open <code>Session</code> in hibernate's <code>SessionFactory</code>
+     */
+    protected Session getOpenSession() {
+        return sessionFactory.openSession();
+    }
 }
