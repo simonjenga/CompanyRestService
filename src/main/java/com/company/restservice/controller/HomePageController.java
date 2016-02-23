a9589e10251e824208775fca5eccb328d1d8f209
Simon N <simtuje@gmail.com>
2016-02-23 10:40:00 +0300

    formatting code & javadocs

--- src/main/java/com/company/restservice/controller/HomePageController.java
+++ src/main/java/com/company/restservice/controller/HomePageController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * This spring controller class renders and displays the home page of the application.
+ *
+ * @author Simon Njenga
+ * @since 0.1
+ */
 @Controller
 public class HomePageController {
 
