d28c678168a441a2b8f7a193e17cfbe4f81b81c8
Simon N <simtuje@gmail.com>
2016-03-31 14:09:15 +0300

    update the two files by mistake

--- src/test/java/com/company/restservice/controller/CompanyControllerTest.java
+++ src/test/java/com/company/restservice/controller/CompanyControllerTest.java
@@ -1,26 +1,24 @@
 package com.company.restservice.controller;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
@@ -41,47 +39,43 @@ import com.company.restservice.model.Owner;
  * @author Simon Njenga
  * @since 0.1
  */
-//@org.junit.Ignore
+@Ignore
+@Transactional
 @WebAppConfiguration
-@RunWith(SpringJUnit4ClassRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@EnableTransactionManagement(proxyTargetClass = true)
-@TestExecutionListeners(value = DependencyInjectionTestExecutionListener.class, inheritListeners = true)
+@RunWith(value = SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { HibernateConfiguration.class }, loader = AnnotationConfigWebContextLoader.class)
-@Transactional(transactionManager = "transactionManager", propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, readOnly = false)
+@ComponentScan(basePackages = "com.company.restservice")
+@TestExecutionListeners(DependencyInjectionTestExecutionListener.class)
 public class CompanyControllerTest {
 
+	public static final String REST_SERVICE_URI = "http://localhost:8080/CompanyRestService/restservice";
+	
 	@Autowired
 	private CompanyDAO companyDAO;
 	
 	private Company company;
 	private Owner ownerOne;
 	
-	private String baseURL;
-    private RestTemplate template;
-	
 	@Before
 	public void setup() {
-		this.company = new Company();
-		this.ownerOne = new Owner();
-		
-		this.baseURL = "http://localhost:8080/CompanyRestService/restservice";
-        this.template = new RestTemplate();
+		company = new Company();
+		ownerOne = new Owner();
 				
-        this.company.setName("Johnathan");
-        this.company.setAddress("Chelsea");
-        this.company.setCity("London");
-        this.company.setCountry("England");
-        this.company.setEmail("johnathan@chelsea.sports");
-        this.company.setPhoneNumber("+44-867-023-9514");
+		company.setName("Johnathan");
+		company.setAddress("Chelsea");
+		company.setCity("London");
+		company.setCountry("England");
+		company.setEmail("johnathan@chelsea.sports");
+		company.setPhoneNumber("+44-867-023-9514");
 		
-        this.ownerOne.setName("Owner One");
+		ownerOne.setName("Owner One");
 		
-		List<Owner> owners = new ArrayList<Owner>(1);
-		owners.add(this.ownerOne);
+		List<Owner> owners = new ArrayList<Owner>(2);
+		owners.add(ownerOne);
 		
-		this.company.setOwner(owners);
-		this.ownerOne.setCompany(this.company);
+		company.setOwner(owners);
+		ownerOne.setCompany(company);
 	}
     
     /**
@@ -90,35 +84,14 @@ public class CompanyControllerTest {
      * @throws Exception If some problem inside
      */
 	@Test
-    @Rollback(true)
+    @SuppressWarnings("unchecked")
     public void testGetCompanyList() {
 		// save the company to database
-		Company savedCompany = companyDAO.saveCompany(company);
+		companyDAO.saveCompany(company);
 		
-		Assert.assertTrue(savedCompany.getId() != null);
-		
-		ResponseEntity<String> response = this.template.getForEntity(this.baseURL + "/companies", String.class);
+        RestTemplate restTemplate = new RestTemplate();
+        List<LinkedHashMap<String, Object>> companiesMap = restTemplate.getForObject(REST_SERVICE_URI + "/companies", List.class);
         
-		Assert.assertTrue(response != null && response.hasBody() && !response.getBody().isEmpty());
-		System.out.println(response.getBody().toString());
-    }
-	
-	/**
-     * This test is for HTTP GET request for the web service. 
-     * 
-     * @throws Exception If some problem inside
-     */
-	@Test
-    @Rollback(true)
-    public void testGetCompany() {
-		// save the company to database
-		Company savedCompany = companyDAO.saveCompany(company);
-		
-		Assert.assertTrue(savedCompany.getId() != null);
-		
-		ResponseEntity<String> response = this.template.getForEntity(this.baseURL + "/company/" + savedCompany.getId(), String.class);
-        
-		Assert.assertTrue(response != null && response.hasBody() && !response.getBody().isEmpty());
-		System.out.println(response.getBody().toString());
+        Assert.assertTrue(companiesMap != null && !companiesMap.isEmpty() && companiesMap.size() > 0);
     }
 }
