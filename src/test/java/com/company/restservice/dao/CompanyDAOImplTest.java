27c374ee5c7a1d7c90e10fc5283dab7e0066bb9e
Simon N <simtuje@gmail.com>
2016-04-02 09:45:00 +0300

    polishing on CompanyDAOImplTest

--- src/test/java/com/company/restservice/dao/CompanyDAOImplTest.java
+++ src/test/java/com/company/restservice/dao/CompanyDAOImplTest.java
@@ -30,6 +30,7 @@ import com.company.restservice.model.Owner;
  * @author Simon Njenga
  * @since 0.1
  */
+@Rollback
 @WebAppConfiguration
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(value = SpringJUnit4ClassRunner.class)
@@ -42,11 +43,11 @@ public class CompanyDAOImplTest {
 	@Autowired
 	private CompanyDAO companyDAO;
 	
-	private Company company;
+	private Company company, savedCompany;
 	private Owner ownerOne, ownerTwo;
 	
 	@Before
-	public void setup() {
+	public void setUp() throws Exception {
 		this.company = new Company();
 		this.ownerOne = new Owner();
 		this.ownerTwo = new Owner();
@@ -68,106 +69,5 @@ public class CompanyDAOImplTest {
 		this.company.setOwner(owners);
 		this.ownerOne.setCompany(this.company);
 		this.ownerTwo.setCompany(this.company);
-	}
-	
-	/**
-     * JUnit tests documentation to be implemented later!.
-     *  
-     * @throws Exception If some problem inside
-     */
-	@Test
-    @Rollback(true)
-	public void testAddCompany() {
-		// save the company to database
-		Company savedCompany = this.companyDAO.saveCompany(this.company);
-		
-		Assert.assertTrue(savedCompany.getId() != null);
-		Assert.assertEquals(this.company.getName(), savedCompany.getName());
-		Assert.assertTrue(savedCompany.getOwner().size() == 2);
-	}
-	
-	/**
-     * JUnit tests documentation to be implemented later!.
-     *  
-     * @throws Exception If some problem inside
-     */
-	@Test
-    @Rollback(true)
-	public void testUpdateCompany() {
-		// save the company to database
-		Company savedCompany = this.companyDAO.saveCompany(this.company);
-		savedCompany.setName("Felicity");
-		savedCompany.setAddress("Salisbury");
-		savedCompany.setCity("Wiltshire");
-		savedCompany.setCountry("England");
-		savedCompany.setEmail("felicity@salisbury.com");
-		savedCompany.setPhoneNumber("+44-753-110-9524");
-		
-		Company updatedCompany = this.companyDAO.updateCompany(savedCompany);
-		
-		Assert.assertTrue(savedCompany.getId() != null);
-		Assert.assertNotEquals(updatedCompany.getName(), "Elizabeth");
-		Assert.assertEquals(updatedCompany.getName(), "Felicity");
-		Assert.assertTrue(updatedCompany.getEmail().equals("felicity@salisbury.com"));
-	}
-	
-	/**
-     * JUnit tests documentation to be implemented later!.
-     *  
-     * @throws Exception If some problem inside
-     */
-	@Test
-    @Rollback(true)
-	public void testCompaniesList() {
-		// save the company to database
-		Company savedCompany = this.companyDAO.saveCompany(this.company);
-		
-		List<Company> companiesList = this.companyDAO.getCompaniesList();
-		
-		Assert.assertTrue(savedCompany.getId() != null && companiesList.size() != 0);
-	}
-	
-	/**
-     * JUnit tests documentation to be implemented later!.
-     *  
-     * @throws Exception If some problem inside
-     */
-	@Test
-    @Rollback(true)
-	public void testUpdateCompanyWithNewOwner() {
-		// save the company to database
-		Company savedCompany = this.companyDAO.saveCompany(this.company);
-		savedCompany.setName("Felicity");
-		savedCompany.setAddress("Salisbury");
-		savedCompany.setCity("Wiltshire");
-		savedCompany.setCountry("England");
-		savedCompany.setEmail("felicity@salisbury.com");
-		savedCompany.setPhoneNumber("+44-753-110-9524");
-		
-		Owner ownerThree = new Owner();
-		ownerThree.setName("Owner Three");
-		
-		List<Owner> owners = new ArrayList<Owner>(1);
-		owners.add(ownerThree);	
-		
-		savedCompany.setOwner(owners);
-		ownerThree.setCompany(savedCompany);
-		
-		this.companyDAO.updateCompany(savedCompany);
-		
-		Company updatedCom = this.companyDAO.getCompanyById(savedCompany.getId());
-		
-		Assert.assertTrue(!savedCompany.getId().equals(null));
-		Assert.assertNotEquals(updatedCom.getName(), "Elizabeth");
-		Assert.assertEquals(updatedCom.getName(), "Felicity");
-		Assert.assertTrue(updatedCom.getEmail().equals("felicity@salisbury.com"));
-		
-		List<Owner> allOwners = updatedCom.getOwner();
-		
-		Assert.assertTrue(allOwners.size() == 3);
-		Assert.assertTrue(!allOwners.get(2).equals(null));
-		Assert.assertTrue(allOwners.get(2).getName().equals("Owner Three"));
-		Assert.assertTrue(allOwners.get(2).getCompany().getAddress().equals("Salisbury"));
-		Assert.assertTrue(allOwners.get(2).getCompany().getPhoneNumber().equals("+44-753-110-9524"));		
 	}
 }
