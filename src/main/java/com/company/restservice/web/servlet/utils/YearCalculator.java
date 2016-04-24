cfbfa4efa531cfb650389be8c26daf54f1ea95a7
Simon N <simtuje@gmail.com>
2016-04-24 20:50:00 +0300

    indentation and documentation fixes

--- src/main/java/com/company/restservice/web/servlet/utils/YearCalculator.java
+++ src/main/java/com/company/restservice/web/servlet/utils/YearCalculator.java
@@ -10,23 +10,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Documentation for this class will be done later!
+ * This subclass of {@code HttpServlet} creates an HTTP servlet that computes the current year
+ * and the output is printed on the footer of every JSP page.
  * 
  * @author Simon Njenga
  * @since 0.1
  */
 public class YearCalculator extends HttpServlet {
 
-	/**
+    /**
      * Serialization marker.
      */
-	private static final long serialVersionUID = 5473151833849926992L;
+    private static final long serialVersionUID = 5473151833849926992L;
 
-	public void doGet(HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException {
-		res.setContentType("text/html");
-		Calendar cal = Calendar.getInstance();
-		PrintWriter out = res.getWriter();
-		out.print(cal.get(Calendar.YEAR));
-	}
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+        throws IOException, ServletException {
+
+        res.setContentType("text/html");
+        Calendar cal = Calendar.getInstance();
+        PrintWriter out = res.getWriter();
+        out.print(cal.get(Calendar.YEAR));
+    }
+
+    public void destroy() {
+        // Intentionally empty!
+    }
 }
