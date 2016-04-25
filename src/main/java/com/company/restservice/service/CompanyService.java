6a1c58ad7e3575d82aad5debeaa3349b5cc4b01b
Simon N <simtuje@gmail.com>
2016-04-25 11:15:00 +0300

    indentation and documentation fixes

--- src/main/java/com/company/restservice/service/CompanyService.java
+++ src/main/java/com/company/restservice/service/CompanyService.java
@@ -5,18 +5,42 @@ import java.util.List;
 import com.company.restservice.model.Company;
 
 /**
- * Documentation for this class will be done later!
- * 
+ * Service interface class.
+ *  
  * @author Simon Njenga
- * @since 0.1
+ * @version 0.1
  */
 public interface CompanyService {
 
-	Company saveCompany(Company company);
-	
-	Company updateCompany(Company company, Company companyToUpdate);
-	
-	List<Company> getCompanyList();
-	
-	Company getCompanyById(Long companyId);
+    /**
+     * Saves a <code>Company</code> object to the database.
+     * 
+     * @param <code>Company</code>company, company object to save.
+     * @return the <code>Company</code> object that has been saved in the database
+     */
+    Company saveCompany(Company company);
+
+    /**
+     * Updates a <code>Company</code> object in the database.
+     * 
+     * @param <code>Company</code>company, company object that has the update details.
+     * @param <code>Company</code>companyToUpdate, company object that needs to be updated.
+     * @return the <code>Company</code> object that has been updated from the database
+     */
+    Company updateCompany(Company company, Company companyToUpdate);
+
+    /**
+     * Retrieves a list of all <code>Company</code> objects from the database.
+     * 
+     * @return a <code>List</code> of <code>Company</code> objects
+     */
+    List<Company> getCompanyList();
+
+    /**
+     * Retrieves a <code>Company</code> object from the database.
+     * 
+     * @param <code>Long</code>companyId, companyId of the <code>Company</code> object to retrieve.
+     * @return a <code>Company</code> object
+     */
+    Company getCompanyById(Long companyId);
 }
