26e7433f276eaa31b2267c7b246d4e42ee0bd78d
Simon N <simtuje@gmail.com>
2016-04-28 15:10:00 +0300

    indentation and documentation fixes

--- src/main/java/com/company/restservice/dao/CompanyDAO.java
+++ src/main/java/com/company/restservice/dao/CompanyDAO.java
@@ -5,20 +5,49 @@ import java.util.List;
 import com.company.restservice.model.Company;
 
 /**
- * Documentation for this class will be done later!
- * 
+ * This class is the Data Access Object (DAO) for the {@code Company} entity
+ * in the domain model.
+ *  
  * @author Simon Njenga
- * @since 0.1
+ * @version 0.1
  */
 public interface CompanyDAO {
 
-	Company saveCompany(Company company);
-	
-	Company updateCompany(Company company);
-	
-	Company getCompanyById(Long companyId);
-	
-	void deleteCompany(Long companyId);
-	
-	List<Company> getCompaniesList();
+    /**
+     * Saves a <code>Company</code> object to the database.
+     * 
+     * @param <code>Company</code>company, company object to save.
+     * @return the <code>Company</code> object that has been saved in the database
+     */
+    Company saveCompany(Company company);
+
+    /**
+     * Updates a <code>Company</code> object in the database.
+     * 
+     * @param <code>Company</code>company, company object that needs to be updated.
+     * @return the <code>Company</code> object that has been updated from the database
+     */
+    Company updateCompany(Company company);
+
+    /**
+     * Retrieves a <code>Company</code> object from the database.
+     * 
+     * @param <code>Long</code>companyId, companyId of the <code>Company</code> object to retrieve.
+     * @return a <code>Company</code> object
+     */
+    Company getCompanyById(Long companyId);
+
+    /**
+     * Deletes a <code>Company</code> object from the database.
+     * 
+     * @param <code>Long</code>companyId, companyId of the <code>Company</code> object to delete.
+     */
+    void deleteCompany(Long companyId);
+
+    /**
+     * Retrieves a list of all <code>Company</code> objects from the database.
+     * 
+     * @return a <code>List</code> of <code>Company</code> objects
+     */
+    List<Company> getCompaniesList();
 }
