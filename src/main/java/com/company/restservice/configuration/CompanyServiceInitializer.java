997e3bef8d73ab09376a540efb93ac8c9f801b3d
Simon N <simtuje@gmail.com>
2016-04-30 09:40:00 +0300

    indentation and documentation fixes

--- src/main/java/com/company/restservice/configuration/CompanyServiceInitializer.java
+++ src/main/java/com/company/restservice/configuration/CompanyServiceInitializer.java
@@ -7,31 +7,38 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 import com.company.restservice.web.servlet.utils.CORSFilter;
 
 /**
- * Documentation for this class will be done later!
+ * This class is a concrete implementation of the {@link AbstractAnnotationConfigDispatcherServletInitializer}
+ * which in itself is an implementation of the {@link WebApplicationInitializer} interface.
  * 
+ * This class helps simplifying Java-based Spring configuration as we don't need to manually create contexts but
+ * just set appropriate configuration classes in {@link #getRootConfigClasses()} and {@link #getServletConfigClasses()} 
+ * methods.
+ *
+ * http://www.kubrynski.com/2014/01/understanding-spring-web-initialization.html
+ *
  * @author Simon Njenga
  * @since 0.1
  */
 public class CompanyServiceInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
- 
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[] { CompanyServiceConfiguration.class };
     }
-  
+
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return null;
     }
-  
+
     @Override
     protected String[] getServletMappings() {
         return new String[] { "/", "/restservice/*" };
     }
-    
+
     @Override
     protected Filter[] getServletFilters() {
-    	Filter [] singleton = { new CORSFilter() };
-    	return singleton;
-	} 
+        Filter [] singleton = { new CORSFilter() };
+        return singleton;
+    }
 }
