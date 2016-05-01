86bb7ad977152fd8433d38a0c79f1cb306616138
Simon N <simtuje@gmail.com>
2016-05-01 20:10:00 +0300

    indentation and documentation fixes

--- src/main/java/com/company/restservice/configuration/CompanyWebApplicationInitializer.java
+++ src/main/java/com/company/restservice/configuration/CompanyWebApplicationInitializer.java
@@ -8,20 +8,24 @@ import org.springframework.web.WebApplicationInitializer;
 import com.company.restservice.web.servlet.utils.YearCalculator;
 
 /**
- * Documentation for this class will be done later!
+ * This class is an implementation of the {@link WebApplicationInitializer} interface.
  * 
+ * This class helps simplifying Java-based Spring configuration in Servlet 3.0+ environments in order to configure
+ * the {@link ServletContext} programmatically as opposed to (or possibly in conjunction with) the traditional
+ * web.xml-based approach.
+ *
+ * http://docs.spring.io/spring/docs/current/spring-framework-reference/html/mvc.html
+ * http://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/web/WebApplicationInitializer.html
+ *
  * @author Simon Njenga
  * @since 0.1
  */
 public class CompanyWebApplicationInitializer implements WebApplicationInitializer {
 
-	@Override
-	public void onStartup(ServletContext container) {
-		ServletRegistration.Dynamic registration = container.addServlet("yearCalc", new YearCalculator());
-		registration.setLoadOnStartup(1);
-		registration.addMapping("/theYear");
-	}
+    @Override
+    public void onStartup(ServletContext container) {
+        ServletRegistration.Dynamic registration = container.addServlet("yearCalc", new YearCalculator());
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/theYear");
+    }
 }
-
-// http://docs.spring.io/spring/docs/current/spring-framework-reference/html/mvc.html
-// http://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/web/WebApplicationInitializer.html
