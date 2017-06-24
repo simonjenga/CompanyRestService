c392bfea42817781b5ede8891be79fc14f25acb8
Simon N <simtuje@gmail.com>
2017-06-24 09:45:00 +0300

    update to home page controller mapping

--- src/main/java/com/company/restservice/controller/HomePageController.java
+++ src/main/java/com/company/restservice/controller/HomePageController.java
@@ -3,7 +3,6 @@ package com.company.restservice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * This spring controller class renders and displays the home page of the application.
@@ -15,7 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomePageController {
 
     @RequestMapping(value = "/homePage.htm", method = RequestMethod.GET)  
-    public ModelAndView getHomePage() {
-        return new ModelAndView("/companyManagement");
+    public String getHomePage() {
+        return "companyManagement";
     }
 }
