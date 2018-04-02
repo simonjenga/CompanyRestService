80927a06b203739191403a5c69219f86eb839c11
Simon N <simtuje@gmail.com>
2018-04-02 15:10:00 +0300

    conversion of class from java to groovy

---
+++ src/main/java/com/company/restservice/web/servlet/utils/YearCalculator.groovy
@@ -0,0 +1,21 @@
+package com.company.restservice.web.servlet.utils;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This subclass of {@code HttpServlet} creates an HTTP servlet that computes the current year
+ * and the output is printed on the footer of every JSP page.
+ * 
+ * @author Simon Njenga
+ * @since 0.1
+ */
+public class YearCalculator extends HttpServlet {
+
+}
